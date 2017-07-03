282739d9a76e890682a277561e6124571e78f437
zikalino <zikalino@microsoft.com>
2017-07-03 09:26:15 +0800

    added generic sample code

---
+++ AzureIoTHubConnectedServiceLibrary/Resources/C/generic/azure_iot_hub.c
@@ -0,0 +1,346 @@
+﻿
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "azure_c_shared_utility/platform.h"
+#include "azure_c_shared_utility/threadapi.h"
+#include "iothub_client.h"
+#include "iothubtransportmqtt.h"
+#include "jsondecoder.h"
+#include "azure_iot_hub.h"
+
+
+//
+// String containing Hostname, Device Id & Device Key in the format:
+// "HostName=<host_name>;DeviceId=<device_id>;SharedAccessKey=<device_key>"
+//
+// Note: this connection string is specific to the device "$deviceId$". To configure other devices,
+// see information on iothub-explorer at http://aka.ms/iothubgetstartedVSCS
+//
+static const char* connection_string = "HostName=$iotHubUri$;DeviceId=$deviceId$;SharedAccessKey=$deviceKey$";
+
+const char *onSuccess = "\"Successfully invoked device method\"";
+const char *notFound = "\"No method found\"";
+
+const char *fieldDeviceId = ";DeviceId=";
+const char *fieldSharedAccessKey = ";SharedAccessKey=";
+//
+// To monitor messages sent to device "$deviceId$" use iothub-explorer as follows:
+//    iothub-explorer HostName=$iotHubUri$;SharedAccessKeyName=service;SharedAccessKey=$deviceKey$ monitor-events "$deviceId$"
+//
+
+// Refer to http://aka.ms/azure-iot-hub-vs-cs-wiki for more information on Connected Service for Azure IoT Hub
+
+const char AzureIoTCertificatesX[] =
+/* Baltimore */
+"-----BEGIN CERTIFICATE-----\r\n"
+"MIIDdzCCAl+gAwIBAgIEAgAAuTANBgkqhkiG9w0BAQUFADBaMQswCQYDVQQGEwJJ\r\n"
+"RTESMBAGA1UEChMJQmFsdGltb3JlMRMwEQYDVQQLEwpDeWJlclRydXN0MSIwIAYD\r\n"
+"VQQDExlCYWx0aW1vcmUgQ3liZXJUcnVzdCBSb290MB4XDTAwMDUxMjE4NDYwMFoX\r\n"
+"DTI1MDUxMjIzNTkwMFowWjELMAkGA1UEBhMCSUUxEjAQBgNVBAoTCUJhbHRpbW9y\r\n"
+"ZTETMBEGA1UECxMKQ3liZXJUcnVzdDEiMCAGA1UEAxMZQmFsdGltb3JlIEN5YmVy\r\n"
+"VHJ1c3QgUm9vdDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAKMEuyKr\r\n"
+"mD1X6CZymrV51Cni4eiVgLGw41uOKymaZN+hXe2wCQVt2yguzmKiYv60iNoS6zjr\r\n"
+"IZ3AQSsBUnuId9Mcj8e6uYi1agnnc+gRQKfRzMpijS3ljwumUNKoUMMo6vWrJYeK\r\n"
+"mpYcqWe4PwzV9/lSEy/CG9VwcPCPwBLKBsua4dnKM3p31vjsufFoREJIE9LAwqSu\r\n"
+"XmD+tqYF/LTdB1kC1FkYmGP1pWPgkAx9XbIGevOF6uvUA65ehD5f/xXtabz5OTZy\r\n"
+"dc93Uk3zyZAsuT3lySNTPx8kmCFcB5kpvcY67Oduhjprl3RjM71oGDHweI12v/ye\r\n"
+"jl0qhqdNkNwnGjkCAwEAAaNFMEMwHQYDVR0OBBYEFOWdWTCCR1jMrPoIVDaGezq1\r\n"
+"BE3wMBIGA1UdEwEB/wQIMAYBAf8CAQMwDgYDVR0PAQH/BAQDAgEGMA0GCSqGSIb3\r\n"
+"DQEBBQUAA4IBAQCFDF2O5G9RaEIFoN27TyclhAO992T9Ldcw46QQF+vaKSm2eT92\r\n"
+"9hkTI7gQCvlYpNRhcL0EYWoSihfVCr3FvDB81ukMJY2GQE/szKN+OMY3EU/t3Wgx\r\n"
+"jkzSswF07r51XgdIGn9w/xZchMB5hbgF/X++ZRGjD8ACtPhSNzkE1akxehi/oCr0\r\n"
+"Epn3o0WC4zxe9Z2etciefC7IpJ5OCBRLbf1wbWsaY71k5h+3zvDyny67G7fyUIhz\r\n"
+"ksLi4xaNmjICq44Y3ekQEe5+NauQrz4wlHrQMz2nZQ/1/I6eYs9HRCwBXbsdtTLS\r\n"
+"R9I4LtD+gdwyah617jzV/OeBHRnDJELqYzmp\r\n"
+"-----END CERTIFICATE-----\r\n"
+/* MSIT */
+"-----BEGIN CERTIFICATE-----\r\n"
+"MIIFhjCCBG6gAwIBAgIEByeaqTANBgkqhkiG9w0BAQsFADBaMQswCQYDVQQGEwJJ\r\n"
+"RTESMBAGA1UEChMJQmFsdGltb3JlMRMwEQYDVQQLEwpDeWJlclRydXN0MSIwIAYD\r\n"
+"VQQDExlCYWx0aW1vcmUgQ3liZXJUcnVzdCBSb290MB4XDTEzMTIxOTIwMDczMloX\r\n"
+"DTE3MTIxOTIwMDY1NVowgYsxCzAJBgNVBAYTAlVTMRMwEQYDVQQIEwpXYXNoaW5n\r\n"
+"dG9uMRAwDgYDVQQHEwdSZWRtb25kMR4wHAYDVQQKExVNaWNyb3NvZnQgQ29ycG9y\r\n"
+"YXRpb24xFTATBgNVBAsTDE1pY3Jvc29mdCBJVDEeMBwGA1UEAxMVTWljcm9zb2Z0\r\n"
+"IElUIFNTTCBTSEEyMIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEA0eg3\r\n"
+"p3aKcEsZ8CA3CSQ3f+r7eOYFumqtTicN/HJq2WwhxGQRlXMQClwle4hslAT9x9uu\r\n"
+"e9xKCLM+FvHQrdswbdcaHlK1PfBHGQPifaa9VxM/VOo6o7F3/ELwY0lqkYAuMEnA\r\n"
+"iusrr/466wddBvfp/YQOkb0JICnobl0JzhXT5+/bUOtE7xhXqwQdvDH593sqE8/R\r\n"
+"PVGvG8W1e+ew/FO7mudj3kEztkckaV24Rqf/ravfT3p4JSchJjTKAm43UfDtWBpg\r\n"
+"lPbEk9jdMCQl1xzrGZQ1XZOyrqopg3PEdFkFUmed2mdROQU6NuryHnYrFK7sPfkU\r\n"
+"mYsHbrznDFberL6u23UykJ5jvXS/4ArK+DSWZ4TN0UI4eMeZtgzOtg/pG8v0Wb4R\r\n"
+"DsssMsj6gylkeTyLS/AydGzzk7iWa11XWmjBzAx5ihne9UkCXgiAAYkMMs3S1pbV\r\n"
+"S6Dz7L+r9H2zobl82k7X5besufIlXwHLjJaoKK7BM1r2PwiQ3Ov/OdgmyBKdHJqq\r\n"
+"qcAWjobtZ1KWAH8Nkj092XA25epCbx+uleVbXfjQOsfU3neG0PyeTuLiuKloNwnE\r\n"
+"OeOFuInzH263bR9KLxgJb95KAY8Uybem7qdjnzOkVHxCg2i4pd+/7LkaXRM72a1o\r\n"
+"/SAKVZEhZPnXEwGgCF1ZiRtEr6SsxwUQ+kFKqPsCAwEAAaOCASAwggEcMBIGA1Ud\r\n"
+"EwEB/wQIMAYBAf8CAQAwUwYDVR0gBEwwSjBIBgkrBgEEAbE+AQAwOzA5BggrBgEF\r\n"
+"BQcCARYtaHR0cDovL2N5YmVydHJ1c3Qub21uaXJvb3QuY29tL3JlcG9zaXRvcnku\r\n"
+"Y2ZtMA4GA1UdDwEB/wQEAwIBhjAdBgNVHSUEFjAUBggrBgEFBQcDAQYIKwYBBQUH\r\n"
+"AwIwHwYDVR0jBBgwFoAU5Z1ZMIJHWMys+ghUNoZ7OrUETfAwQgYDVR0fBDswOTA3\r\n"
+"oDWgM4YxaHR0cDovL2NkcDEucHVibGljLXRydXN0LmNvbS9DUkwvT21uaXJvb3Qy\r\n"
+"MDI1LmNybDAdBgNVHQ4EFgQUUa8kJpz0aCJXgCYrO0ZiFXsezKUwDQYJKoZIhvcN\r\n"
+"AQELBQADggEBAHaFxSMxH7Rz6qC8pe3fRUNqf2kgG4Cy+xzdqn+I0zFBNvf7+2ut\r\n"
+"mIx4H50RZzrNS+yovJ0VGcQ7C6eTzuj8nVvoH8tWrnZDK8cTUXdBqGZMX6fR16p1\r\n"
+"xRspTMn0baFeoYWTFsLLO6sUfUT92iUphir+YyDK0gvCNBW7r1t/iuCq7UWm6nnb\r\n"
+"2DVmVEPeNzPR5ODNV8pxsH3pFndk6FmXudUu0bSR2ndx80oPSNI0mWCVN6wfAc0Q\r\n"
+"negqpSDHUJuzbEl4K1iSZIm4lTaoNKrwQdKVWiRUl01uBcSVrcR6ozn7eQaKm6ZP\r\n"
+"2SL6RE4288kPpjnngLJev7050UblVUfbvG4=\r\n"
+"-----END CERTIFICATE-----\r\n"
+/* *.azure-devices.net */
+"-----BEGIN CERTIFICATE-----\r\n"
+"MIIGcjCCBFqgAwIBAgITWgABtrNbz7vBeV0QWwABAAG2szANBgkqhkiG9w0BAQsF\r\n"
+"ADCBizELMAkGA1UEBhMCVVMxEzARBgNVBAgTCldhc2hpbmd0b24xEDAOBgNVBAcT\r\n"
+"B1JlZG1vbmQxHjAcBgNVBAoTFU1pY3Jvc29mdCBDb3Jwb3JhdGlvbjEVMBMGA1UE\r\n"
+"CxMMTWljcm9zb2Z0IElUMR4wHAYDVQQDExVNaWNyb3NvZnQgSVQgU1NMIFNIQTIw\r\n"
+"HhcNMTUwODI3MDMxODA0WhcNMTcwODI2MDMxODA0WjAeMRwwGgYDVQQDDBMqLmF6\r\n"
+"dXJlLWRldmljZXMubmV0MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA\r\n"
+"nXC/qBUdlnfIm5K3HYu0o/Mb5tNNcsr0xy4Do0Puwq2W1tz0ZHvIIS9VOANhkNCb\r\n"
+"VyOncnP6dvmM/rYYKth/NQ8RUiZOYlROZ0SYC8cvxq9WOln4GXtEU8vNVqJbYrJj\r\n"
+"rPMHfxqLzTE/0ZnQffnDT3iMUE9kFLHow0YgaSRU0KZsc9KAROmzBzu+QIB1WGKX\r\n"
+"D7CN361tG1UuN68Bz7MSnbgk98Z+DjDxfusoDhiiy/Y9MLOJMt4WIy5BqL3lfLnn\r\n"
+"r+JLqmpiFuyVUDacFQDprYJ1/AFgcsKYu/ydmASARPzqJhOGaC2sZP0U5oBOoBzI\r\n"
+"bz4tfn8Bi0kJKmS53mQt+wIDAQABo4ICOTCCAjUwCwYDVR0PBAQDAgSwMB0GA1Ud\r\n"
+"JQQWMBQGCCsGAQUFBwMBBggrBgEFBQcDAjAdBgNVHQ4EFgQUKpYehBSNA53Oxivn\r\n"
+"aLCz3+eFUJ0wXQYDVR0RBFYwVIITKi5henVyZS1kZXZpY2VzLm5ldIIaKi5hbXFw\r\n"
+"d3MuYXp1cmUtZGV2aWNlcy5uZXSCISouc3UubWFuYWdlbWVudC1henVyZS1kZXZp\r\n"
+"Y2VzLm5ldDAfBgNVHSMEGDAWgBRRryQmnPRoIleAJis7RmIVex7MpTB9BgNVHR8E\r\n"
+"djB0MHKgcKBuhjZodHRwOi8vbXNjcmwubWljcm9zb2Z0LmNvbS9wa2kvbXNjb3Jw\r\n"
+"L2NybC9tc2l0d3d3Mi5jcmyGNGh0dHA6Ly9jcmwubWljcm9zb2Z0LmNvbS9wa2kv\r\n"
+"bXNjb3JwL2NybC9tc2l0d3d3Mi5jcmwwcAYIKwYBBQUHAQEEZDBiMDwGCCsGAQUF\r\n"
+"BzAChjBodHRwOi8vd3d3Lm1pY3Jvc29mdC5jb20vcGtpL21zY29ycC9tc2l0d3d3\r\n"
+"Mi5jcnQwIgYIKwYBBQUHMAGGFmh0dHA6Ly9vY3NwLm1zb2NzcC5jb20wTgYDVR0g\r\n"
+"BEcwRTBDBgkrBgEEAYI3KgEwNjA0BggrBgEFBQcCARYoaHR0cDovL3d3dy5taWNy\r\n"
+"b3NvZnQuY29tL3BraS9tc2NvcnAvY3BzADAnBgkrBgEEAYI3FQoEGjAYMAoGCCsG\r\n"
+"AQUFBwMBMAoGCCsGAQUFBwMCMA0GCSqGSIb3DQEBCwUAA4ICAQCrjzOSW+X6v+UC\r\n"
+"u+JkYyuypXN14pPLcGFbknJWj6DAyFWXKC8ihIYdtf/szWIO7VooplSTZ05u/JYu\r\n"
+"ZYh7fAw27qih9CLhhfncXi5yzjgLMlD0mlbORvMJR/nMl7Yh1ki9GyLnpOqMmO+E\r\n"
+"yTpOiE07Uyt2uWelLHjMY8kwy2bSRXIp7/+A8qHRaIIdXNtAKIK5jo068BJpo77h\r\n"
+"4PljCb9JFdEt6sAKKuaP86Y+8oRZ7YzU4TLDCiK8P8n/gQXH0vvhOE/O0n7gWPqB\r\n"
+"n8KxsnRicop6tB6GZy32Stn8w0qktmQNXOGU+hp8OL6irULWZw/781po6d78nmwk\r\n"
+"1IFl2TB4+jgyblvJdTM0rx8vPf3F2O2kgsRNs9M5qCI7m+he43Bhue0Fj/h3oIIo\r\n"
+"Qx7X/uqc8j3VTNE9hf2A4wksSRgRydjAYoo+bduNagC5s7Eucb4mBG0MMk7HAQU9\r\n"
+"m/gyaxqth6ygDLK58wojSV0i4RiU01qZkHzqIWv5FhhMjbFwyKEc6U35Ps7kP/1O\r\n"
+"fdGm13ONaYqDl44RyFsLFFiiDYxZFDSsKM0WDxbl9ULAlVc3WR85kEBK6I+pSQj+\r\n"
+"7/Z5z2zTz9qOFWgB15SegTbjSR7uk9mEVnj9KDlGtG8W1or0EGrrEDP2CMsp0oEj\r\n"
+"VTJbZAxEaZ3cVCKva5sQUxFMjwG32g==\r\n"
+"-----END CERTIFICATE-----\r\n";
+
+static char* retrieveKeys();
+static void sendMessageCallback(IOTHUB_CLIENT_CONFIRMATION_RESULT result, void* context);
+static IOTHUBMESSAGE_DISPOSITION_RESULT receiveMessageCallback(IOTHUB_MESSAGE_HANDLE message, void* context);$deviceTwinCallbackDecl$$deviceMethodCallbackDecl$
+
+#define MAX_CONNECTION_STRING_SIZE 128
+
+static IOTHUB_CLIENT_LL_HANDLE iothub_client_handle = NULL;
+
+/// <summary>
+/// Establishes a connection to Azure IoT Hub.
+/// </summary>
+bool clientConnect(void)
+{
+    if (platform_init() != 0)
+    {
+        Log_Debug("ERROR: failed initializing platform.\n");
+        return false;
+    }
+
+    char* updatedConnectionString = retrieveKeys();
+
+    if (updatedConnectionString == NULL)
+    {
+        Log_Debug("ERROR: failed to allocate buffer for connection string\n");
+        return false;
+    }
+
+    iothub_client_handle = IoTHubClient_LL_CreateFromConnectionString(updatedConnectionString, MQTT_Protocol);
+
+    free(updatedConnectionString);
+
+    if (iothub_client_handle == NULL)
+    {
+        return false;
+    }
+
+	IOTHUB_CLIENT_RESULT azureRes = IoTHubClient_LL_SetOption(iothub_client_handle, "TrustedCerts", AzureIoTCertificatesX);
+
+	if (azureRes != IOTHUB_CLIENT_OK)
+	{
+		Log_Debug("ERROR: failure to set option \"TrustedCerts\"\n");
+		return false;
+	}
+
+	// set callbacks
+	IoTHubClient_LL_SetMessageCallback(iothub_client_handle, receiveMessageCallback, NULL);
+$deviceMethodCallbackRegistration$$deviceTwinCallbackRegistration$
+
+	return true;
+}
+
+static char* retrieveKeys()
+{
+    char* updated = (char*)malloc(MAX_CONNECTION_STRING_SIZE * 3);
+
+    if (updated != NULL)
+    {
+        if (strstr(connection_string, "=%s") != NULL)
+        {
+            char* deviceId = updated + MAX_CONNECTION_STRING_SIZE;
+            char* sharedAccessKey = deviceId + MAX_CONNECTION_STRING_SIZE;
+
+            if (Config_ReadString("DeviceId", deviceId, MAX_CONNECTION_STRING_SIZE) == Result_Success)
+            {
+                if (Config_ReadString("SharedAccessKey", sharedAccessKey, MAX_CONNECTION_STRING_SIZE) == Result_Success)
+                {
+                    snprintf(updated, MAX_CONNECTION_STRING_SIZE, connection_string, deviceId, sharedAccessKey);
+                    return updated;
+                }
+                else
+                {
+                    Log_Debug("ERROR: failed to get SharedAccessKey\n");
+                }
+            }
+            else
+            {
+                Log_Debug("ERROR: failed to get DeviceId\n");
+            }
+
+        }
+        else
+        {
+            Log_Debug("Using embedded key\n");
+            strncpy(updated, connection_string, MAX_CONNECTION_STRING_SIZE);
+            return updated;
+        }
+
+        free(updated);
+    }
+
+    return NULL;
+}
+
+/// <summary>
+/// Disconnect and destroys all resources.
+/// </summary>
+void clientDisconnect(void)
+{
+	if (iothub_client_handle != NULL)
+	{
+		IoTHubClient_LL_Destroy(iothub_client_handle);
+		iothub_client_handle = NULL;
+
+		platform_deinit();
+	}
+}
+
+/// <summary>
+/// Performs IoT Hub client processing.
+/// </summary>
+void doWork(void)
+{
+	Log_Debug("doWork called...\n");
+	IoTHubClient_LL_DoWork(iothub_client_handle);
+}
+
+/// <summary>
+/// Sends single message to IoT Hub.
+/// </summary>
+void sendMessage(void)
+{
+	if (iothub_client_handle == NULL)
+	{
+		Log_Debug("ERROR: IoT Hub client not initialized\n");
+	}
+	else
+	{
+		const unsigned char* message = "{{\"deviceId\":\"mydevice\",\"messageId\":1,\"text\":\"Hello Cloud from a C app!\"}}";
+
+		IOTHUB_MESSAGE_HANDLE message_handle = IoTHubMessage_CreateFromByteArray(message, strlen(message));
+
+		if (message_handle == 0)
+		{
+			Log_Debug("ERROR: unable to create a new IoTHubMessage\n");
+		}
+		else
+		{
+			if (IoTHubClient_LL_SendEventAsync(iothub_client_handle, message_handle, sendMessageCallback, /*&callback_param*/0) != IOTHUB_CLIENT_OK)
+			{
+				Log_Debug("ERROR: failed to hand over the message to IoTHubClient");
+			}
+			else
+			{
+				Log_Debug("IoTHubClient accepted the message for delivery\n");
+			}
+		}
+	}
+}
+
+$deviceTwinCodeReported$
+
+/// <summary>
+/// Callback when sending message to IoT Hub is completed.
+/// </summary>
+static void sendMessageCallback(IOTHUB_CLIENT_CONFIRMATION_RESULT result, void* context)
+{
+	Log_Debug("Message received by IoT Hub. Result is: %d\n", result);
+}
+
+/// <summary>
+/// Callback when message is received from IoT Hub.
+/// </summary>
+static IOTHUBMESSAGE_DISPOSITION_RESULT receiveMessageCallback(IOTHUB_MESSAGE_HANDLE message, void* context)
+{
+	const unsigned char* buffer;
+	size_t size;
+	if (IoTHubMessage_GetByteArray(message, &buffer, &size) != IOTHUB_MESSAGE_OK)
+	{
+		Log_Debug("ERROR: unable to IoTHubMessage_GetByteArray\n");
+	}
+	else
+	{
+		/*buffer is not zero terminated*/
+		unsigned char* str_msg = malloc(size + 1);
+
+		if (str_msg != NULL)
+		{
+			memcpy(str_msg, buffer, size);
+			str_msg[size] = '\0';
+
+			Log_Debug("Received message '%s' from IoT Hub\n", str_msg);
+			free(str_msg);
+		}
+		else
+		{
+			Log_Debug("ERROR: couldn't allocate buffer for received message\n");
+		}
+	}
+
+	return IOTHUBMESSAGE_ACCEPTED;
+}
+
+$deviceMethodCode$
+
+$deviceTwinCodeDesired$
+
+/// <summary>
+/// Main demo entry point.
+/// </summary>
+void runDemo()
+{
+	int counter = 60;
+
+	clientConnect();
+	sendMessage(); $twinReportStateCall$
+
+		while (counter--)
+		{
+			// send message every 5 seconds
+			if (counter % 5 == 0)
+				sendMessage();
+
+			doWork();
+			ThreadAPI_Sleep(1000);
+		}
+
+	clientDisconnect();
+}
---
+++ AzureIoTHubConnectedServiceLibrary/Resources/C/generic/azure_iot_hub.h
@@ -0,0 +1,44 @@
+﻿/// \file config.h
+/// \brief This header defines a sample interface for performing basic operations with Azure
+/// IoT Hub
+///
+/// Add #include "azure_iot_hub.h" in your project.
+/// Call runDemo() to run entire demo or call specific function to perform specific operations.
+#pragma once
+
+/// <summary>
+///     Runs entire demo, depending on the configuration:
+///      - connection to IoT Hub
+///      - sending messages
+///      - receiving messages
+///      - handling direct method calls
+///      - updating device twin
+///      - receiving device twin updates
+/// </summary>
+void runDemo();
+
+/// <summary>
+///     Establishes connection to Azure IoT Hub
+/// </summary>
+bool clientConnect();
+
+/// <summary>
+///     Disconnect from Azure IoT Hub and releases resources
+/// </summary>
+void clientDisconnect();
+
+/// <summary>
+///     Reports device state using device twin
+/// </summary>
+void twinReportState();
+
+/// <summary>
+///     Sends sample message.
+/// </summary>
+void sendMessage();
+
+/// <summary>
+///     Keeps IoT Hub Client alive.
+///     Call this function periodically.
+/// </summary>
+void doWork();
