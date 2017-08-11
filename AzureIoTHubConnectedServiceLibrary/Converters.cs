8d6943e644a0a0ca0110dbafa3a43654784ae17f
zikalino <zikalino@microsoft.com>
2017-08-11 10:23:00 +0800

    C2D message now works

---
+++ AzureIoTHubConnectedServiceLibrary/Converters.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace AzureIoTHubConnectedService
+{
+    public class StringToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return !string.IsNullOrWhiteSpace(System.Convert.ToString(value));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
